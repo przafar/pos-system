a108fd8bfcdb9829503f3a0342ff0829fb0309b1
Zafar <su10409@studenti.polito.it>
2025-08-18 22:49:05 +0500

    ref: initial

--- src/features/cart/lib/initial.ts
+++ src/features/cart/lib/initial.ts
@@ -1,18 +1,18 @@
 import type { CartItem } from "../types"; 
 
 export const initialCart: CartItem[] = [
-  {
-    id: "4780058470616",
-    name: "Alkogolsiz ichimliklar PET 1,5 l",
-    price: 12000,
-    qty: 2,
-    unit: "dona",
+  { 
+    id: "4780058470616", 
+    name: "Moxito qulupnay 0.5l", 
+    price: 12000, 
+    unit: "dona", 
+    qty: 3
   },
   {
     id: "4780058470616-2",
-    name: "Noski detskie",
+    name: "Kvas russkiy 0.5l",
     price: 3000,
-    qty: 4,
     unit: "dona",
+    qty: 2
   },
 ];
